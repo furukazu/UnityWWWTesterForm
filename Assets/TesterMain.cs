89d26fa82327e9e690663a0bd9227046425cbd83
furukazu <yuki.ohiro@gmail.com>
2015-08-05 10:40:46 +0900

    init

---
+++ Assets/TesterMain.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TesterMain : MonoBehaviour {
+
+	public InputField ConMethod;
+	public InputField ConURL;
+	public InputField ReqHeader;
+	public InputField ReqBody;
+	public InputField ResContent;
+
+	public void OnSubmitPushed(){
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
